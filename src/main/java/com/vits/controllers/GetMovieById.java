79248e70afeb197648d770186b5c28184a96f774
vitoria <vitoriabcarvalhocorrea@gmail.com>
2023-10-28 13:15:31 -0300

    make getMovieById call the movieById template

--- src/main/java/com/vits/controllers/GetMovieById.java
+++ src/main/java/com/vits/controllers/GetMovieById.java
@@ -3,22 +3,31 @@ package com.vits.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.vits.models.Movie;
 import com.vits.repositories.MovieRepository;
 
-@RestController
+@Controller
 @RequestMapping("/movies")
 public class GetMovieById {
 	@Autowired 
 	private MovieRepository repo;
 	
 	@GetMapping("/{id}")
-	public Optional<Movie> getMovieById(@PathVariable Integer id){
-		return repo.findById(id);
+	public String getMovieById(@PathVariable Integer id, Model model){
+		Optional<Movie> movieOptional = repo.findById(id);
+		if (movieOptional.isPresent()) {
+		    Movie movie = movieOptional.get(); 
+		    model.addAttribute("movie", movie);
+		    return "tv-by-id";
+		}
+		else {
+			return "index";
+		}
 	}
 }
